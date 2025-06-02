c46129a3fa67469ade1ac218f0b5315cbe3e0d6d
haroldevs <haroldthomasg18@gmail.com>
2025-06-01 22:58:18 -0500

    chore: api

--- src/components/Atendidos.jsx
+++ src/components/Atendidos.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
-import { bus } from "common-utils";
+import React, { useEffect, useState } from 'react'
+import { bus, apiGetAtenciones } from "common-utils";
 
 const Atendidos = () => {
   const [submit, setSubmit] = useState(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const handleSubmit =  () => {
+  const handleSubmit = () => {
     setSubmit(!submit);
     bus.next({
       topic: 'atendidos',
@@ -17,8 +17,19 @@ const Atendidos = () => {
         }
       }
     });
-    bus
   }
+
+  const [atenciones, setAtenciones] = useState([]);
+
+  const getData = async () => {
+    const data = await apiGetAtenciones({})
+    setAtenciones(data.items);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <button onClick={handleSubmit} >enviar data</button>
